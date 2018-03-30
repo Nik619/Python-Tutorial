20d59c92bdacd692735089e452c0f64946f7ec81
Nik619 <nikhilnebula@gmail.com>
2018-03-30 16:53:21 +0530

    Update Helloworld.py

--- Helloworld.py
+++ Helloworld.py
@@ -1,5 +1,8 @@
-a = "HELLO"
+# A Simple Hello World application
 
+# Assiging String
+a = "HELLO"
 b =  "World"
 
+#The + operator is used to concatinate the two String that was assigned to A and B
 print(a + b );
