2eeb78ba65eeac3cc9eb7cc9444b4c53d768afe5
Nik619 <nikhilnebula@gmail.com>
2018-10-21 12:34:21 +0530

    Update Ifelse.py

--- Ifelse.py
+++ Ifelse.py
@@ -1,5 +1,8 @@
+# We have declared a value called name which take in user input.
 name = raw_input("Name ?")
 
+# The if Name matches with any of condition set it would return the Name that we have enterted if not will return a not a 
+# Correct Name
 if name == "mark":
 	print("the name entered is", name)
 elif name == "john":
