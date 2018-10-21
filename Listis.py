1f5d2132632b7fcf6898a2af5627156b08a66330
Nik619 <nikhilnebula@gmail.com>
2018-10-21 12:38:02 +0530

    Update Listis.py

--- Listis.py
+++ Listis.py
@@ -1,5 +1,5 @@
 
-#this is used to print String and NUmber  from a List in Python
+# This Code block is used to print String or Number from a List using Python
 
 # below example is for printing a String and Selecting values from a String
 str = 'Helloworld'
@@ -9,13 +9,14 @@ print (str[0])
 print (str[2:5])
 print (str[0:2])
 
-#The can be done on a list
+# The next example we have created a list name lists and smalllist.
 
 
 lists = ['ABd',2,33.45,"John"]
 
 smalllist = ['JOJO',45]
 
+# The output would print values starting from 0 to 2
 print(lists[0:2])
 
 
